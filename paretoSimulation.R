4ae62037f95c000dfdc568ef5dd197df99b8462b
Daniel Salnikov <73490811+dansal182@users.noreply.github.com>
2021-01-03 13:37:16 -0600

    Update paretoSimulation.R

--- paretoSimulation.R
+++ paretoSimulation.R
@@ -1,8 +1,10 @@
+##Pareto sample simulation     
 Y <- rpareto( 300, 5, 3/2)
 summary(Y)
 
 quantile(Y, probs = seq(0,1, 0.05))
 
+##Plots to visualize the data
 par(mfrow = c(1,3))
 meanLifePlot(Y)
 plot(ecdf(Y), col="blue", main = "Empirical Distribution of Y", xlab = "y", ylab = "Fn(y)" )
@@ -18,6 +20,7 @@ qqplot(rexp(200, 2), log(Y[Y>=30]), pch = 19, main = "Log Data QQ-Plot", ylab ="
 qqnorm(Y, pch = 19)
 par(mfrow=c(1,1))
 
+##Threshold search for the Pareto sample
 threshs <- c(10, 20, 25, 30, 35, 40)
 K = 6
 probs = rep(1/K, K)
@@ -36,7 +39,7 @@ betsMean = c( mean(threshSearch[[1]][1,-(1:500)]), mean(threshSearch[[1]][2,-(1:
 betsMean
 sum(threshs*betsMean)
 
-
+##Hypothesis tests for the Pareto sample
 Yextr <- Y[Y >= 30]
 alphaBY(Yextr, 32, 1,1)
 m = 500
@@ -61,8 +64,10 @@ plot(Hy[-(1:7500),1], type = "l")
 quantile(Hy[-(1:7500),1])
 ##Clearly these data have a Heavy-Tail
 
+##True value of the tail index alpha
 trueA <- 3/2
 
+##Posterior distribution simulation for the Pareto sample
 MCMCy <- MHRWChain(10000,  c(5,2,0), Y[Y>=32], 32, c(1,1,1,1), 50) 
 summary(MCMCy)
 
